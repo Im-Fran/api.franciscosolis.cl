16dc048c087d1ed2ec46d08ce709403ac1726191
Francisco Solis <30329003+Im-Fran@users.noreply.github.com>
2023-10-27 22:46:34 -0300

    feat: added helper

---
+++ app/Helpers/Helper.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Helpers;
+
+class Helper {
+
+    static function repeat(int $times, callable $callback): void {
+        foreach (range(0, $times) as $_) {
+            $callback();
+        }
+    }
+}
